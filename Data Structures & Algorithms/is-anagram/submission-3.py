6bab67d49222623c7d115be62029c2b71f3e91d2
BradleyE99 <hpt4xx@virginia.edu>
2026-06-08 09:58:48 -0400

    Update: is-anagram - submission-3

--- Data Structures & Algorithms/is-anagram/submission-3.py
+++ Data Structures & Algorithms/is-anagram/submission-3.py
@@ -1,9 +1,27 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        s_sorted = sorted(s)
-        t_sorted = sorted(t)
-
-        if (s_sorted == t_sorted):
-            return True
-        else:
+        if len(s) != len(t):
             return False
+
+        s_map = {}
+        t_map = {}
+
+
+        for c in s:
+            if c in s_map:
+                s_map[c] += 1
+            else:
+                s_map[c] = 1
+        for v in t:
+            if v in t_map:
+                t_map[v] += 1
+            else:
+                t_map[v] = 1
+
+        return s_map == t_map       
+
+            
+
+
+
+       
