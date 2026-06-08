eaaed5be905892ade7c7eada32eb1a36493b5c1c
BradleyE99 <hpt4xx@virginia.edu>
2026-06-08 10:08:15 -0400

    Update: anagram-groups - submission-0

--- Data Structures & Algorithms/anagram-groups/submission-0.py
+++ Data Structures & Algorithms/anagram-groups/submission-0.py
@@ -1,11 +1,12 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        result = {}
+        temp_map = {}
 
-        for i in strs:
-            key = "".join(sorted(i))
-            if (key not in result):
-                result[key] = []                
-            result[key].append(i)
-
-        return list(result.values())
+        for c in strs:
+            if ''.join(sorted(c)) in temp_map:
+                temp_map[''.join(sorted(c))].append(c)
+            else:
+                temp_map[''.join(sorted(c))] = [c]
+        print(temp_map)
+   
+        return list(temp_map.values())
